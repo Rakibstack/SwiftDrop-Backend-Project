21ae46ac73667d89c5e9fd1c5974e66e2b16e9bf
Rakibstack <rh6039511@gmail.com>
2026-09-19 21:56:13 +0600

    feat(middleware):Implement checkAuth,globalErrorHandler and validationMiddleware

---
+++ src/app/middleware/checkAuth.ts
@@ -0,0 +1,76 @@
+
+import type { NextFunction, Request, Response } from "express";
+import type { JwtPayload } from "jsonwebtoken";
+import config from "../config";
+import { prisma } from "../lib/prisma";
+import { catchAsync } from "../utils/catchAsync";
+import { jwtUtils } from "../utils/jwt";
+import AppError from "../utils/AppError";
+import httpstatus from "http-status";
+import { UserRole } from "../../generated/prisma/enums";
+
+export interface requestUser {
+  email: string;
+  name: string;
+  userId: string;
+  role: UserRole;
+}
+declare global {
+  namespace Express {
+    interface Request {
+      user?: requestUser;
+    }
+  }
+}
+
+
+export const auth = (...requiredRoles: UserRole[]) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const token = req.cookies.accessToken
+      ? req.cookies.accessToken
+      : req.headers.authorization?.startsWith("Bearer ")
+        ? req.headers.authorization?.split(" ")[1]
+        : req.headers.authorization;
+
+    if (!token) {
+throw new AppError(httpstatus.UNAUTHORIZED, "You are not logged in. Please log in to access this resource.");
+    }
+
+    const verifiedToken = jwtUtils.verifyToken(token, config.jwt_access_secret);
+
+    if (!verifiedToken.success) {
+      throw new AppError(httpstatus.UNAUTHORIZED, verifiedToken.error);
+    }
+
+    const { email, name, userId, role } = verifiedToken.data as JwtPayload;
+
+    if (requiredRoles.length && !requiredRoles.includes(role)) {
+throw new AppError(httpstatus.FORBIDDEN, "Forbidden. You don't have permission to access this resource.");
+    }
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+        email,
+        role,
+      },
+    });
+
+    if (!user) {
+      throw new AppError(httpstatus.UNAUTHORIZED, "User not found. Please log in again.");
+    }
+
+    if (user.status === "SUSPENDED") {
+      throw new AppError(httpstatus.FORBIDDEN, "Your account has been blocked. Please contact support.");
+    }
+
+    req.user = {
+      email,
+      name,
+      userId,
+      role,
+    };
+
+    next();
+  });
+};
---
+++ src/app/middleware/validationMiddleware.ts
@@ -0,0 +1,22 @@
+
+
+import { NextFunction, Request, Response } from "express";
+import { catchAsync } from "../utils/catchAsync";
+import z from "zod";
+import AppError from "../utils/AppError";
+import httpstatus from "http-status";
+
+export const validationRequest = (zodSchema: z.ZodObject) => {
+  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const payload = req.body ?? {};
+
+    const result = zodSchema.safeParse(payload);
+    if (!result.success) {
+      throw new AppError(httpstatus.BAD_REQUEST, result.error.issues[0].message);
+    }
+
+    req.body = result.data
+    
+    next()
+  });
+};
